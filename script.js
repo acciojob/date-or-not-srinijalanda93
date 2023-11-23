326175ea1f7d1ff25ad996790ec12809a502c68f
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-11-23 18:36:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var isDate = function (input) {
   //   write your code here
+	return !isNaN(new Date(input));
 };
 
+// Do not change the code below.
+const input = prompt("Enter Date.");
+alert(isDate(input));
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
