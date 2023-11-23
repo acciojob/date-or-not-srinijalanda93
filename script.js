edcb3f1eb79a5d28638591cf4d0d9cc0d37d8b12
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-11-23 18:39:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,3 @@ var isDate = function (input) {
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
-// Do not change the code below.
-const input = prompt("Enter Date.");
-alert(isDate(input));
